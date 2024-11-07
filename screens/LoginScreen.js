924f3245f904cea11fc05db3ef2ecc509b9b028f
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-11-07 16:00:12 +0100

    feat: add signup/login try/catch Alert

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,28 @@
 import AuthContent from '../components/Auth/AuthContent';
+import { useState } from 'react';
+import LoadingOverlay from '../UI/LoadingOverlay';
+import { login } from '../util/auth';
+import { Alert } from 'react-native';
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function loginHandler({email, password}) {
+    setIsAuthenticating(true);
+    
+    try {
+      await login(email, password);
+    } catch (error) {
+      Alert.alert('Authetincation failed!', 'Could not log you in. Please chack your credentials again or try again later!');
+    }
+    setIsAuthenticating(false);
+  }
+
+  if(isAuthenticating){
+    return <LoadingOverlay message="Logging you in..." />
+  }
+
+  return <AuthContent isLogin onAuthenticate={loginHandler} />;
 }
 
 export default LoginScreen;
